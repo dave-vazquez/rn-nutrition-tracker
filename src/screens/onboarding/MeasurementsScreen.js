ec293267555b5b93898a9fe9c03ffb734a949ab3
Dave Vazquez <dmvaz.li@gmail.com>
2021-01-01 00:29:33 -0500

    replaces yup validation schema with rhf rules

--- src/screens/onboarding/MeasurementsScreen.js
+++ src/screens/onboarding/MeasurementsScreen.js
@@ -1,21 +1,22 @@
 import { Context as OnboardingContext } from "_contexts/OnboardingContext.js";
 import g from "_globalstyles";
+import { isAfter, isBefore } from "date-fns";
 import React, { useContext } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { StyleSheet, View } from "react-native";
 import { Input } from "react-native-elements";
 import VMasker from "vanilla-masker";
-import { yupResolver } from "@hookform/resolvers/yup";
+import isValidDate from "../../util/isValidDate";
 import { NextButton, OnboardingView } from "./components";
-import { measurementsSchema } from "./validationSchemas";
+
+const DATE_MAX = new Date().setFullYear(new Date().getFullYear() - 13);
+const DATE_MIN = new Date().setFullYear(new Date().getFullYear() - 100);
 
 const MeasurementsScreen = ({ navigation: { navigate } }) => {
   const { updateMeasurements } = useContext(OnboardingContext);
 
   const { handleSubmit, control, errors } = useForm({
     mode: "onBlur",
-    shouldFocusError: true,
-    resolver: yupResolver(measurementsSchema),
   });
 
   const onSubmit = (measurements) => {
@@ -36,12 +37,14 @@ const MeasurementsScreen = ({ navigation: { navigate } }) => {
           name="heightFt"
           control={control}
           defaultValue=""
-          render={({ onChange, value }) => (
+          rules={rules.heightFt}
+          render={({ onChange, onBlur, value }) => (
             <Input
               label="Height"
-              value={String(value)}
+              value={value}
               placeholder="ft."
               errorStyle={s.error}
+              onBlur={onBlur}
               errorMessage={errors.heightFt?.message}
               keyboardType="numeric"
               onChangeText={onChange}
@@ -57,11 +60,13 @@ const MeasurementsScreen = ({ navigation: { navigate } }) => {
           name="heightIn"
           control={control}
           defaultValue=""
-          render={({ onChange, value }) => (
+          rules={rules.heightIn}
+          render={({ onChange, onBlur, value }) => (
             <Input
-              value={String(value)}
+              value={value}
               placeholder="in."
               errorStyle={s.error}
+              onBlur={onBlur}
               errorMessage={errors.heightIn?.message}
               keyboardType="numeric"
               onChangeText={onChange}
@@ -77,11 +82,13 @@ const MeasurementsScreen = ({ navigation: { navigate } }) => {
           name="weightLbs"
           control={control}
           defaultValue=""
-          render={({ onChange, value }) => (
+          rules={rules.weightLbs}
+          render={({ onChange, onBlur, value }) => (
             <Input
               label="Weight"
-              value={String(value)}
+              value={value}
               placeholder="lbs."
+              onBlur={onBlur}
               errorStyle={s.error}
               errorMessage={errors.weightLbs?.message}
               keyboardType="numeric"
@@ -98,11 +105,13 @@ const MeasurementsScreen = ({ navigation: { navigate } }) => {
           name="dateOfBirth"
           control={control}
           defaultValue=""
-          render={({ onChange, value }) => (
+          rules={rules.dateOfBirth}
+          render={({ onChange, onBlur, value }) => (
             <Input
               label="Date of Birth"
               value={value}
               errorStyle={s.error}
+              onBlur={onBlur}
               errorMessage={errors.dateOfBirth?.message}
               placeholder="mm/dd/yyyy"
               keyboardType="numeric"
@@ -119,6 +128,69 @@ const MeasurementsScreen = ({ navigation: { navigate } }) => {
       <NextButton gutterTop={20} onPress={handleSubmit(onSubmit)} />
     </OnboardingView>
   );
+};
+
+const rules = {
+  heightFt: {
+    required: {
+      value: true,
+      message: "Required field",
+    },
+    min: {
+      value: 2,
+      message: "Are you sure? The world record is 1'11\"",
+    },
+    max: {
+      value: 9,
+      message: "Are you sure? The world record is 8'11\"",
+    },
+  },
+  heightIn: {
+    required: {
+      value: true,
+      message: "Required field",
+    },
+    min: {
+      value: 0,
+      message: "Inches should be at least 0",
+    },
+    max: {
+      value: 11,
+      message: "Inches should be less than 12",
+    },
+  },
+  weightLbs: {
+    required: {
+      value: true,
+      message: "Required field",
+    },
+    min: {
+      value: 66,
+      message: "Weight should be at least 66 lbs",
+    },
+    max: {
+      value: 1399,
+      message: "This weight is not supported",
+    },
+  },
+  dateOfBirth: {
+    required: {
+      value: true,
+      message: "Required field",
+    },
+    validate: {
+      isValid: (dateOfBirth) =>
+        isValidDate(dateOfBirth) ? true : "Invalid Date",
+      min: (dateOfBirth) =>
+        isAfter(new Date(dateOfBirth), DATE_MIN)
+          ? true
+          : "This age is not supported",
+      max: (dateOfBirth) =>
+        isBefore(new Date(dateOfBirth), DATE_MAX)
+          ? true
+          : "Must be at least 13 years old",
+    },
+  },
 };
 
 const s = StyleSheet.create({
