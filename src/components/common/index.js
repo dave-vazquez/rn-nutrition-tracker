ccda8010a3d556ee0f81256d3559cd3b1b5b5394
Dave Vazquez <dmvaz.li@gmail.com>
2021-01-14 13:34:58 -0500

    creates reuseable card component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
 export { default as RadioButton } from "./RadioButton";
 export { default as NextButton } from "./NextButton";
 export { default as Spacer } from "./Spacer";
+export { default as Card } from "./Card";
