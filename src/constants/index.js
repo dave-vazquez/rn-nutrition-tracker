a80375d96f4dba0ba21d098a64dc807ac31bbd10
Dave Vazquez <dmvaz.li@gmail.com>
2021-01-23 12:56:32 -0500

    creates constants file for values used across application

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+// _screens/onboarding/ActivityLevelScreen.js
+export const NOT_ACTIVE = 1.2;
+export const LIGHT_ACTIVE = 1.375;
+export const MODERATE_ACTIVE = 1.465;
+export const ACTIVE = 1.55;
+export const VERY_ACTIVE = 1.725;
+export const EXTREMELY_ACTIVE = 1.9;
+
+// _components/flows/foodsearch/FoodLogForm.js
+export const MEAL_TYPES = ["Breakfast", "Lunch", "Dinner", "Snack"];
