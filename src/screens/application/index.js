fc73e7ff467bf40e1ee184144961699700f626c5
Dave Vazquez <dmvaz.li@gmail.com>
2021-02-14 12:08:21 -0500

    moves application-scoped screens to individual directories

--- src/screens/application/index.js
+++ src/screens/application/index.js
@@ -1,6 +1,6 @@
-export { default as JournalScreen } from "./JournalScreen";
-export { default as BarcodeScreen } from "./BarcodeScreen";
+export { default as JournalScreen } from "./journal/JournalScreen";
+export { default as BarcodeScreen } from "./barcode/BarcodeScreen";
 export { default as FoodSearchScreen } from "./foodsearch/FoodSearchScreen";
-export { default as ProgressScreen } from "./ProgressScreen";
-export { default as SettingsScreen } from "./SettingsScreen";
+export { default as ProgressScreen } from "./progress/ProgressScreen";
+export { default as SettingsScreen } from "./settings/SettingsScreen";
 export { default as FoodDetailsScreen } from "./foodsearch/FoodDetailsScreen";
