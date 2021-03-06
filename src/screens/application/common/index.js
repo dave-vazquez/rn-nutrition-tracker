6a1f2424bb6854824b1f54704e80e2ba371c6101
Dave Vazquez <dmvaz.li@gmail.com>
2021-03-06 16:25:36 -0500

    removes unused component

--- src/screens/application/common/index.js
+++ src/screens/application/common/index.js
@@ -1,3 +1,2 @@
 export { default as DailyBudgetsCard } from "./DailyBudgetsCard/DailyBudgetsCard";
-export { default as NutritionSummary } from "./DailyBudgetsCard/NutritionSummary";
 export { default as HeaderBottom } from "./HeaderBottom";
